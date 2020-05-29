b69fa83eb34c19ef5cc323bad71058d698839d61
Stanko Culaja <culaja@gmail.com>
2020-05-29 16:20:13 +0200

    Added initial code. need to test it.

---
+++ TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace TestApp
+{
+    class Program
+    {
+        private static readonly GpioController Controller = new GpioController();
+        
+        static void Main()
+        {
+            Controller.OpenPin(1, PinMode.Input);
+            Controller.OpenPin(2, PinMode.Output);
+            Controller.RegisterCallbackForPinValueChangedEvent(1, PinEventTypes.None, Callback);
+            
+            Console.ReadLine();
+            
+            Controller.UnregisterCallbackForPinValueChangedEvent(1, Callback);
+            Controller.Dispose();
+        }
+
+        private static void Callback(object sender, PinValueChangedEventArgs args)
+        {
+            Console.WriteLine($"Received '{args.ChangeType}' signal on pin '{args.PinNumber}'");
+        }
+    }
+}

4b8fd1f0a3a6da081d7e4f2e8b869eb20ed74e6b
Stanko Culaja <stanko.culaja@kolotree.com>
2020-09-24 17:32:42 +0200

    Added some code.

---
+++ Adapters/LiteQueueAdapter/PinSnapshotQueue.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Framework;
+using LiteQueue;
+using Ports;
+
+namespace LiteQueueAdapter
+{
+    internal sealed class PinSnapshotQueue : IPinSnapshotQueue
+    {
+        private readonly LiteQueue<PinSnapshotDto> _liteQueue;
+
+        public PinSnapshotQueue(LiteQueue<PinSnapshotDto> liteQueue)
+        {
+            _liteQueue = liteQueue;
+        }
+        
+        public void Enqueue(PinSnapshot pinSnapshot) => _liteQueue.Enqueue(PinSnapshotDto.From(pinSnapshot));
+
+        public Optional<PinSnapshot> Dequeue()
+        {
+            var queueEntry = _liteQueue.Dequeue(); 
+            return queueEntry.IsCheckedOut
+                ? Optional<PinSnapshot>.None
+                : queueEntry.Payload.ToDomain();
+        }
+    }
+}
---
+++ TestApps/PinSnapshotQueueConsumerTestApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Domain;
+using LiteQueueAdapter;
+
+namespace PinSnapshotQueueConsumerTestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var liteQueueBuilder = LiteQueueBuilder.NewBuilderUsing("TestQueueDatabase.db"))
+            {
+                var pinSnapshotQueue = liteQueueBuilder.PinSnapshotQueueOf("TestQueue");
+
+                while (true)
+                {
+                    var optionalPinSnapshot = pinSnapshotQueue.Dequeue();
+                    if (optionalPinSnapshot.HasNoValue)
+                    {
+                        Console.WriteLine("No items in queue ...");
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        Console.WriteLine(optionalPinSnapshot.Value);
+                    }
+                }
+            }
+        }
+    }
+}
